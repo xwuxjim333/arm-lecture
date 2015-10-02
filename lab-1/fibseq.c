31a14422875c40fc71b6d21b13d85aa9a7849738
xwuxjim333 <xwuxjim333@gmail.com>
2015-10-02 16:29:43 +0800

    Homework1:Fibonacci sequence with recursive method

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
+  
   int number=0;
   int result=0;
+	clock_t start, end;
+  FILE *file = fopen("fib1_add.txt", "a");
 
   scanf("%d",&number);
-  result = fibonacci(number);   
+	start = clock();
+  result = fibonacci(number);
+	end = clock();  
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+
+  if(file){
+	  double total_time = (double)(end - start) / CLOCKS_PER_SEC;
+    fprintf(file, "%d\t%f\n", number, total_time);
+    fclose(file);
+  }   
 }
 
